93de2fe49e8babf0c9f6a96f9f161783fd80ef93
Amani-ATIT <amaniatitallah22951@gmail.com>
2021-08-23 15:55:43 +0100

    first commit

---
+++ src/components/MovieCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactStars from "react-rating-stars-component";
+import {Card} from "react-bootstrap"
+
+const MovieCard = ({Movie}) => {
+    return (
+        <div style={{color:'black'}}>
+   <Card style={{ width: '18rem' }}>
+  <Card.Img alt={Movie.name} variant="top" src={Movie.posterurl} />
+  <Card.Body>
+    <Card.Title>{Movie.name}</Card.Title>
+    <Card.Text>{Movie.description}
+    </Card.Text>
+    <ReactStars
+    count={5}
+    //onChange={ratingChanged}
+    value= {Movie.rating}
+    edit={false}
+    size={24}
+    activeColor="#ffd700"
+  />  </Card.Body>
+</Card>       
+ </div>
+    );
+}
+
+export default MovieCard;
